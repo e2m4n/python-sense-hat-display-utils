ea7ea2a58e1e97f140795962230e5e18702200a3
Aaron Fleming <e_z_a@hotmail.com>
2017-12-10 12:11:13 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sense-hat-display-utils',
+    version='0.1',
+    # packages=[''],
+    url='https://github.com/e2m4n/python-sense-hat-display-utils',
+    license='',
+    author='Aaron Fleming',
+    author_email='e_z_a@hotmail.com',
+    description='Utilities for the LED display on the Raspberry Pi Sense HAT',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='sense-hat',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['sense-hat', 'Pillow', 'colour'],
+    entry_points={
+        'console_scripts': [
+            'sense-hat-display-utils=main:main',
+        ],
+    },
+)
